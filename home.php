02cf93f35ecfc388bc296e6c10d37386e7e07815
Dil Afroja <44971161+DilAfroja@users.noreply.github.com>
2021-03-03 13:56:59 +0600

    project2
    HTML-Bootstrap-php

---
+++ home.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="bg-success">
+<?php
+echo "<h1 class='text-dark bg-success text-center py-1'>WELLCOME </h1><br>";
+echo "<h1 class='text-warning bg-success text-center py-5'> DEMRA UNIVERSITY COLLEGE</h1>";
+?>
+</div>
+</body>
+</html>
